b701fe80e0d21f35755ed455aa145ed498d98064
Josselin <julien.josselin75@hotmail.fr>
2023-12-11 15:07:03 +0100

    add useEffect to Counter

--- src/components/articles/Counter.jsx
+++ src/components/articles/Counter.jsx
@@ -1,9 +1,18 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("La page a chargé");
+  }, []); // crochets vides, signifie au chargement de la page
+
+  useEffect(() => {
+    console.log("count a encore changé");
+  }, [count]); // useEffect écoute count
+  // il sera lancé à chaque changement de count
 
   return (
     <div>
